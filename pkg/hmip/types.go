59c921f182dbef0fe209efa438183838e75d291c
Sascha Gärtner <sascha.gaertner@hdi.de>
2023-12-19 08:13:32 +0100

    extending types

--- pkg/hmip/types.go
+++ pkg/hmip/types.go
@@ -10,13 +10,21 @@ const (
 	EVENT_TYPE_GROUP_CHANGED  = "GROUP_CHANGED"
 	EVENT_TYPE_HOME_CHANGED   = "HOME_CHANGED"
 
-	CHANNEL_TYPE_SWITCH           = "SWITCH_CHANNEL"
-	CHANNEL_TYPE_SWITCH_MEASURING = "SWITCH_MEASURING_CHANNEL"
-	CHANNEL_TYPE_CLIMATE_SENSOR   = "CLIMATE_SENSOR_CHANNEL"
+	CHANNEL_TYPE_DEVICE_BASE       = "DEVICE_BASE"
+	CHANNEL_TYPE_SWITCH            = "SWITCH_CHANNEL"
+	CHANNEL_TYPE_SWITCH_MEASURING  = "SWITCH_MEASURING_CHANNEL"
+	CHANNEL_TYPE_CLIMATE_SENSOR    = "CLIMATE_SENSOR_CHANNEL"
+	CHANNEL_TYPE_ACCESS_CONTROLLER = "ACCESS_CONTROLLER_CHANNEL"
+	CHANNEL_TYPE_SMOKE_DETECTOR    = "SMOKE_DETECTOR_CHANNEL"
 
 	DEVICE_TYPE_TEMPERATURE_HUMIDITY_SENSOR_OUTDOOR = "TEMPERATURE_HUMIDITY_SENSOR_OUTDOOR"
 	DEVICE_TYPE_PLUGABLE_SWITCH                     = "PLUGABLE_SWITCH"
 	DEVICE_TYPE_PLUGABLE_SWITCH_MEASURING           = "PLUGABLE_SWITCH_MEASURING"
+	DEVICE_TYPE_HOME_CONTROL_ACCESS_POINT           = "HOME_CONTROL_ACCESS_POINT"
+	DEVICE_TYPE_SMOKE_DETECTOR                      = "SMOKE_DETECTOR"
+
+	CONNECTION_TYPE_RF  = "HMIP_RF"
+	CONNECTION_TYPE_LAN = "HMIP_LAN"
 
 	GROUP_TYPE_META        = "META"
 	GROUP_TYPE_ENVIRONMENT = "ENVIRONMENT"
@@ -77,13 +85,15 @@ type Group struct {
 }
 
 type Device struct {
-	ID               string                       `json:"id"`
-	Name             string                       `json:"label"`
-	Type             string                       `json:"type"`
-	Model            string                       `json:"modelType"`
-	SGTIN            string                       `json:"serializedGlobalTradeItemNumber"`
-	Channels         map[string]FunctionalChannel `json:"functionalChannels"`
-	LastStatusUpdate HomematicTimestamp           `json:"lastStatusUpdate"`
+	ID                   string                       `json:"id"`
+	Name                 string                       `json:"label"`
+	Type                 string                       `json:"type"`
+	Model                string                       `json:"modelType"`
+	SGTIN                string                       `json:"serializedGlobalTradeItemNumber"`
+	Channels             map[string]FunctionalChannel `json:"functionalChannels"`
+	LastStatusUpdate     HomematicTimestamp           `json:"lastStatusUpdate"`
+	PermanentlyReachable bool                         `json:"permanentlyReachable"`
+	ConnectionType       string                       `json:"connectionType"`
 }
 
 type FunctionalChannel struct {
@@ -93,6 +103,12 @@ type FunctionalChannel struct {
 	VapourAmount            float64 `json:"vaporAmount"`
 	SwitchedOn              bool    `json:"on"`
 	CurrentPowerConsumption float64 `json:"currentPowerConsumption"`
+	LowBattery              bool    `json:"lowBat"`
+	RSSIValue               int     `json:"rssiDeviceValue"`
+	Unreached               bool    `json:"unreach"`
+	Undervoltage            bool    `json:"deviceUndervoltage"`
+	Overheated              bool    `json:"deviceOverheated"`
+	ChamberDegraded         bool    `json:"chamberDegraded"`
 }
 
 type ClientRegistration struct {
